ddcce37a584b85491596cebc7f6f3af4030c963f
Varga Zsombor <zsomborvarga077@gmail.com>
2026-03-20 16:53:31 +0100

    fix: update page content for localization and improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Page() {
       <div className="flex w-full min-h-screen items-center justify-start p-6 md:pl-30">
         <div className="flex w-full max-w-5xl flex-col items-start justify-start gap-6 rounded-lg text-left">
           <h1 className="text-7xl font-bold text-indigo-200 ">Varga Zsombor</h1>
-          <h2 className="text-4xl font- text-indigo-200/20 -mt-1">Software Developer | Web Developer</h2>
+          <h2 className="text-4xl font- text-indigo-200/20 -mt-5">Software Developer | Web Developer</h2>
           <div className="flex w-3xl border border-slate-400"></div>
           <p className="text-slate-200">
-            Explore my projects and experience in web development.
+            Fedezd fel a projektjeimet, ahol a kreativitás és a technológia találkozik.
           </p>
-          <Button variant="default">View Projects</Button>
+          <Button variant="default">Projektek megtekintése</Button>
         </div>
       </div>
     </AuroraBackground>
